1e24600e0b3401e41f83bdc69b7fed2996eceb3d
Hari <cvhari87@gmail.com>
2026-04-10 21:41:51 -0400

    fix: proxy Firebase auth handler to support custom authDomain on iOS PWA

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination: `https://${process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}/__/auth/:path*`,
+      },
+    ]
+  },
 }
 
 export default nextConfig
